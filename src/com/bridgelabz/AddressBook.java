676aa4d37f81438f5e90992931eccd876835f9ca
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-03-27 10:54:33 +0530

    Adding:Edit Contact

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,24 +1,88 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
     public Contact createContact() {
 
-        Contact person = new Contact("pallavi", "kabadi", "wakad", "Pune", "Maharashtra",
-                "pallavikabadi08@gmail.com", 411057, "4353536636");
-        System.out.println("First Name: " + person.getFirstName());
-        System.out.println("Last Name: " + person.getLastName());
-        System.out.println("Address: " + person.getAddress());
-        System.out.println("City: " + person.getCity());
-        System.out.println("State: " + person.getState());
-        System.out.println("Email: " + person.getEmail());
-        System.out.println("Zip Code: " + person.getZipCode());
-        System.out.println("Phone Number: " + person.getPhoneNumber());
-        System.out.println("Contact created successfully.....");
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Contact> addressBook1 = new ArrayList<>();
+
+        Contact person = new Contact();
+        System.out.print("Enter First Name: ");
+        person.setFirstName(sc.next());
+        System.out.print("Enter Last Name: ");
+        person.setLastName(sc.next());
+        System.out.print("Enter Address: ");
+        person.setAddress(sc.next());
+        System.out.print("Enter City: ");
+        person.setCity(sc.next());
+        System.out.print("Enter State: ");
+        person.setState(sc.next());
+        System.out.print("Enter email: ");
+        person.setEmail(sc.next());
+        System.out.print("Enter ZipCode: ");
+        person.setZipCode(sc.nextInt());
+        System.out.print("Enter Phone Number: ");
+        person.setPhoneNumber(sc.nextLong());
+        System.out.println("created new contact");
         return person;
     }
+
     public void addContact() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Contact> addressBook1 = new ArrayList<>();
+
         Contact contact = createContact();
+        addressBook1.add(contact);
         System.out.println(contact);
         System.out.println("Contact added successfully");
+    }
+
+
+    public void editContact() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Contact> addressBook1 = new ArrayList<>();
+
+        boolean isContactFound = false;
+        System.out.println("Enter Name to edit Contact");
+        String name = sc.next();
+        for (Contact contact : addressBook1) {
+            if (name.equalsIgnoreCase(contact.getFirstName())) {
+                isContactFound = true;
+                System.out.print("Enter First Name :");
+                contact.setFirstName(sc.next());
+                System.out.print("Enter Last Name :");
+                contact.setLastName(sc.next());
+                System.out.print("Enter Address :");
+                contact.setAddress(sc.next());
+                System.out.print("Enter City :");
+                contact.setCity(sc.next());
+                System.out.print("Enter State :");
+                contact.setState(sc.next());
+                System.out.print("Enter email :");
+                contact.setEmail(sc.next());
+                System.out.print("Enter ZipCode :");
+                contact.setZipCode(sc.nextInt());
+                System.out.print("Enter Phone Number :");
+                contact.setPhoneNumber(sc.nextLong());
+                System.out.println(contact);
+                break;
+            }
+        }
+        if (isContactFound) {
+            System.out.println("Contact Updated Successfully..");
+        } else {
+            System.out.println("Oops...Contact not found");
+        }
+    }
+
+    void display() {
+        ArrayList<Contact> addressBook1 = new ArrayList<>();
+
+        for (Contact person : addressBook1) {
+            System.out.println(person);
+        }
     }
 }
--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -10,18 +10,9 @@ public class Contact {
     private int zipCode;
     private String phoneNumber;
 
-    public Contact(String firstName, String lastName, String address, String city, String state, String email,
-                         int zipCode, String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.email = email;
-        this.zipCode = zipCode;
-        this.phoneNumber = phoneNumber;
-    }
+    public Contact() {
 
+    }
 
     public String getFirstName() {
         return firstName;
@@ -43,6 +34,10 @@ public class Contact {
         return state;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public int getZipCode() {
         return zipCode;
     }
@@ -51,21 +46,42 @@ public class Contact {
         return phoneNumber;
     }
 
-    public String getEmail() {
-        return email;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = String.valueOf(phoneNumber);
     }
 
     @Override
     public String toString() {
-        return "Contact{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", email='" + email + '\'' +
-                ", zipCode=" + zipCode +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                '}';
+        return "ContactPerson{" + "firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", address='"
+                + address + '\'' + ", city='" + city + '\'' + ", state='" + state + '\'' + ", email='" + email + '\''
+                + ", zipCode=" + zipCode + ", phoneNumber=" + phoneNumber + '}';
     }
 }
